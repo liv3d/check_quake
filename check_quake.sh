47f352371200794149e476688d1da9114d0316b8
Jack-Benny Persson <jake@cyberinfo.se>
2012-02-14 17:37:45 +0100

    Changed grep statement to acually work with qstat

--- check_quake.sh
+++ check_quake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 HOST=$1
 PORT=$2
-STATE=`quakestat -default q2s ${HOST}:${PORT} | grep "q2"`
+STATE=`quakestat -u -default q2s ${HOST}:${PORT} | grep "${HOST}"`
 if [[ $? == 0 ]]; then
 printf "${PORT} - OK\n"
 exit 0
